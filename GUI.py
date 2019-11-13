12073a68298f88e816c0f3e54a617929b85cf3bb
Dr4gon529 <fislabco23@gmail.com>
2019-11-13 16:49:02 +0900

    Box를 다중으로 움직일 수 있게끔 수정해야 함. 현재는 하나만 동작하고 있음

--- GUI.py
+++ GUI.py
@@ -4,13 +4,7 @@ import pygame
 import sys
 
 
-class GUI_information:
-    def __init__(self, screen_width, screen_height, rectangular_size):
-        self.SCREEN_WIDTH = screen_width
-        self.SCREEN_HEIGHT = screen_height
-        self.RECTANGULAR_SIZE = rectangular_size
-        self.DIST = rectangular_size
-
+class color_information:
     BLACK = (0, 0, 0)
     RED = (255, 0, 0)
     GREEN = (0, 255, 0)
@@ -19,7 +13,15 @@ class GUI_information:
     PURPLE = (100, 0, 100)
 
 
-class GUI_screen(GUI_information):
+class GUI_board_information:
+    def __init__(self, screen_width, screen_height, rectangular_size):
+        self.SCREEN_WIDTH = screen_width
+        self.SCREEN_HEIGHT = screen_height
+        self.RECTANGULAR_SIZE = rectangular_size
+        self.DIST = rectangular_size
+
+
+class GUI_screen(GUI_board_information, color_information):
     def __init__(self, screen_width, screen_height, rectangular_size, position_x, position_y):
         super().__init__(screen_width, screen_height, rectangular_size)
         self.position_x = position_x
@@ -35,17 +37,28 @@ class GUI_screen(GUI_information):
         for i in list(dir_l):
             if self.direction == i:
                 if self.position_x + dir_l[i][0]*self.RECTANGULAR_SIZE < 0:
-                    return False
+                    self.direction = 'None'
                 elif self.position_x + dir_l[i][0]*self.RECTANGULAR_SIZE > self.SCREEN_WIDTH-self.RECTANGULAR_SIZE:
-                    return False
+                    self.direction = 'None'
                 elif self.position_y + dir_l[i][1]*self.RECTANGULAR_SIZE < 0:
-                    return False
-                elif self.position_y + dir_l[i][1]*self.RECTANGULAR_SIZE >self.SCREEN_HEIGHT-self.RECTANGULAR_SIZE:
-                    return False
-        return True
+                    self.direction = 'None'
+                elif self.position_y + dir_l[i][1]*self.RECTANGULAR_SIZE > self.SCREEN_HEIGHT-self.RECTANGULAR_SIZE:
+                    self.direction = 'None'
+
+    def edit_position(self):
+        dir_l = {'left': (-1, 0), 'right': (1, 0), 'up': (0, -1), 'down': (0, 1)}
+        self.get_key()
+
+        for i in list(dir_l):
+            if self.direction == i:
+                self.position_x += dir_l[self.direction][0] * self.RECTANGULAR_SIZE
+                self.position_y += dir_l[self.direction][1] * self.RECTANGULAR_SIZE
+
+        self.screen.fill(self.PURPLE)
+        pygame.draw.rect(self.screen, self.YELLOW, (self.position_x, self.position_y, self.RECTANGULAR_SIZE, self.RECTANGULAR_SIZE))
+        pygame.display.update()
 
     def get_key(self):
-        dir_l = {'left': (-1, 0), 'right': (1, 0), 'up': (0, -1), 'down': (0, 1)}
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
@@ -62,22 +75,15 @@ class GUI_screen(GUI_information):
             if event.type == pygame.KEYDOWN and event.key == pygame.K_DOWN:
                 self.direction = 'down'
 
-        if not self.find_out_of_range():
-            self.direction = 'None'
+    def screen_move(self):
+        self.get_key()
+        self.find_out_of_range()
+        if self.direction != 'None':
+            self.edit_position()
 
     def screen_play(self):
-        dir_l = {'left': (-1, 0), 'right': (1, 0), 'up': (0, -1), 'down': (0, 1)}
         while True:
-            self.get_key()
-
-            for i in list(dir_l):
-                if self.direction == i:
-                    self.position_x += dir_l[self.direction][0] * self.RECTANGULAR_SIZE
-                    self.position_y += dir_l[self.direction][1] * self.RECTANGULAR_SIZE
-
-            self.screen.fill(self.PURPLE)
-            pygame.draw.rect(self.screen, self.YELLOW, (self.position_x, self.position_y, self.RECTANGULAR_SIZE, self.RECTANGULAR_SIZE))
-            pygame.display.update()
+            self.screen_move()
 
 
 a = GUI_screen(300, 300, 60, 30, 30)
