4dd1a249d6071b0cf069b90930b8a69264899062
youngchaelee <lyc8170@sasa.hs.kr>
2019-11-27 11:07:10 +0900

    이동할 곳에 랜덤 상자가 생겨서 못 움직이는 부분 고쳐서 올립니다

--- board.py
+++ board.py
@@ -1,7 +1,4 @@
 import random
-from GUI import GUI_management
-from GUI import GUI_key
-from GUI import GUI_screen
 
 class box:#박스 객체 선언
 
@@ -13,7 +10,7 @@ class box:#박스 객체 선언
         self.x = (locate-1)%s
         board[self.y][self.x] = self.num
 
-    def move_left(self):#위로 이동
+    def move_up(self):#위로 이동
         while True:#이동 못할 때 까지 반복
             if board[self.y-1][self.x]==board[self.y][self.x] and self.y>0:#위에 칸이 있고 값이 같으면 병합
                 board[self.y][self.x]=0
@@ -34,7 +31,7 @@ class box:#박스 객체 선언
             else:
                 break
 
-    def move_right(self):#아래로 이동
+    def move_down(self):#아래로 이동
         while True:#이동 못할 때 까지 반복
             if self.y < s-1 and board[self.y+1][self.x]==board[self.y][self.x]:#위에 칸이 있고 값이 같으면 병합
                 board[self.y][self.x]=0
@@ -55,7 +52,7 @@ class box:#박스 객체 선언
             else:
                 break
 
-    def move_down(self):#오른쪽으로 이동
+    def move_right(self):#오른쪽으로 이동
         while True:#이동 못할 때 까지 반복
             if self.x < s-1 and board[self.y][self.x+1]==board[self.y][self.x]:#오른쪽에 칸이 있고 값이 같으면 병합
                 board[self.y][self.x]=0
@@ -76,7 +73,7 @@ class box:#박스 객체 선언
             else:
                 break
 
-    def move_up(self):#왼쪽으로 이동
+    def move_left(self):#왼쪽으로 이동
         while True:#이동 못할 때 까지 반복
             if board[self.y][self.x-1]==board[self.y][self.x] and self.x > 0:#왼쪽에 칸이 있고 비어있으면 왼쪽으로 이동
                 board[self.y][self.x]=0
@@ -111,36 +108,25 @@ def gen_box_locate(): #비어있는 곳을 찾고 그 곳중 랜덤한 위치 �
                 location.append(s*i+j+1)
     return location
 
-def player_move():
-    # 플레이어의 상자 움직이기
-    while True:
-        # 플레이어의 상자 움직이는 방향 입력받음
-        print('direction?')
-        player_move = GUI_key.get_key()
+def player_move():#플레이어의 상자 움직이기
+    while True:#플레이어의 상자 움직이는 방향 입력받음
+        player_move = input('direction?')
 
-        if player_move == 'up':
-            # 위로 움직이는 경우
+        if player_move == 'up':#위로 움직이는 경우
             for i in range(s):
                 for j in range(s):
-                    if board[i][j] is not 0:
-                        # 박스가 있는 칸 조사
+                    if board[i][j] is not 0:#박스가 있는 칸 조사
                         for k in boxes:
-                            if k.locate == s*i+j+1:
-                                # 모든 박스 중에서 위치가 동일한 박스 조사
-                                k.move_up()
-                                # 박스를 위로 이동
+                            if k.locate == s*i+j+1:#모든 박스 중에서 위치가 동일한 박스 조사
+                                k.move_up()#박스를 위로 이동
 
-        if player_move == 'down':
-            # 아래로 움직이는 경우
+        if player_move == 'down':#아래로 움직이는 경우
             for i in range(s-1, -1, -1):
                 for j in range(s):
-                    if board[i][j] is not 0:
-                        # 박스가 있는 칸 조사
+                    if board[i][j] is not 0:#박스가 있는 칸 조사
                         for k in boxes:
-                            if k.locate == s*i+j+1:
-                                # 모든 박스 중에서 위치가 동일한 박스 조사
-                                k.move_down()
-                                # 박스를 아래로 이동
+                            if k.locate == s*i+j+1:#모든 박스 중에서 위치가 동일한 박스 조사
+                                k.move_down()#박스를 아래로 이동
 
         if player_move == 'right':#오른쪽으로 움직이는 경우
             for i in range(s):
@@ -162,34 +148,31 @@ def player_move():
             break
 
 
+
+
+
+
 while True:
-    tmp = GUI_management()
-    while True:
-        # 변 길이 입력받기
-        print("한 변의 크기를 입력해 주세요 (2~8):")
-        s = tmp.show_start_page()
-        if s != 0:
-            break
+    while True:#변 길이 입력받기
+        s = input("한 변의 크기를 입력해 주세요 (2~8):")
+        if is_range_in(s): #s가 2~8까지의 문자인지 판별
+            s = int(s) #정수로 변환
+            break #정수가 아니면 재입력
 
     board = []
 
-    for _ in range(s):
-        # 게임보드 형성
-        board.append([0] * s)
-        # board의 모든 수 0으로 선언(0이 비고 1이 참)
+    for _ in range(s):#게임보드 형성
+        board.append([0] * s) #board의 모든 수 0으로 선언(0이 비고 1이 참)
 
-    boxes = []
+    boxes=[]
 
-    screen = GUI_screen(s)
-    while True:
-        # 상자 생성 - 상자 움직이기 실행
-        boxes.append(box(random.choice([2, 4]), random.choice(gen_box_locate()), 'White'))  # 상자 랜덤한 위치에 생성
+    boxes.append(box(random.choice([2, 4]), random.choice(gen_box_locate()), 'White'))  # 상자 랜덤한 위치에 생성
+
+    while True:#상자 생성 - 상자 움직이기 실행
         for i in range(s):
             for j in range(s):
-                print("%d " % board[i][j], end='')
+                print("%d " %board[i][j],end='')
             print()
-        screen.show_screen(board)
-        player_move()
-        # 방향 입력 받아 움직이기
-
+        player_move()#방향 입력 받아 움직이기
+        boxes.append(box(random.choice([2, 4]), random.choice(gen_box_locate()), 'White'))  # 상자 랜덤한 위치에 생성
 
